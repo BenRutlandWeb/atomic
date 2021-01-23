3de75a5554c0113fdd11351702ce3f8cc107ddf5
Ben Rutland <benrutland@hotmail.com>
2021-01-23 19:12:36 +0000

    added register menu listener

---
+++ app/Listeners/RegisterMenus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+class RegisterMenus
+{
+    /**
+     * Handle the event.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        register_nav_menus([
+            'primary_menu' => __('Primary Menu'),
+            'footer_menu'  => __('Footer Menu'),
+        ]);
+    }
+}
