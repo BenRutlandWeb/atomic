12981a077379b5f165a31be994e1108f7a6cff79
Ben Rutland <benrutland@hotmail.com>
2021-01-24 11:47:46 +0000

    added php code sniffer

--- templates/index.php
+++ templates/index.php
@@ -3,13 +3,12 @@
 wp_head();
 
 if (have_posts()) :
-
-    while (have_posts()) : the_post();
+    while (have_posts()) :
+        the_post();
 
         the_title();
 
         the_content();
-
     endwhile;
 endif;
 
