f057dc2b430268a24d272f752391de2b99931c1e
Ben Rutland <benrutland@hotmail.com>
2021-01-24 10:49:24 +0000

    deleted old view directory and added config comment

--- config/view.php
+++ config/view.php
@@ -2,5 +2,15 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Path
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify a path that should be checked for your views.
+    | WordPress hierarchy templates can be added here insetad of in the theme
+    | root.
+    |
+    */
     'path' => base_path('templates'),
 ];
